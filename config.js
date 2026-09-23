428d35b72fae59fda2f15ec395c1d6ca12ea794c
marianaborgesnutri <mariborges3007@gmail.com>
2026-09-23 19:28:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuração do site de contratos.
+// Troque APENAS o que está entre as aspas. Não apague as aspas nem as vírgulas.
+window.CONFIG = {
+  SUPABASE_URL: "COLE_AQUI_O_PROJECT_URL",
+  SUPABASE_KEY: "COLE_AQUI_A_PUBLISHABLE_KEY"
+};
