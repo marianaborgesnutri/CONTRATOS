c27574b3a656235c4f8815e337fc914c83f78a4f
marianaborgesnutri <mariborges3007@gmail.com>
2026-09-23 19:32:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuração do site de contratos.
 // Troque APENAS o que está entre as aspas. Não apague as aspas nem as vírgulas.
 window.CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_O_PROJECT_URL",
-  SUPABASE_KEY: "COLE_AQUI_A_PUBLISHABLE_KEY"
+  SUPABASE_URL: "https://yjuorotwwytjjadwwbrr.supabase.co",
+  SUPABASE_KEY: "sb_publishable_-yYYT8OwH8CotGE0RAC--g_CzOuM0KN"
 };
